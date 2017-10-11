cf96a7253432a49c417502994370757812b01caf
dlu-umich <31260433+dlu-umich@users.noreply.github.com>
2017-10-11 01:23:41 -0400

    Add files via upload

--- code_hw4.R
+++ code_hw4.R
@@ -14,6 +14,14 @@ plot(fitted(lmod),residuals(lmod),xlab="Fitted Values",ylab="Residuals")
 abline(h=0) # a diagnostic residual plot
 plot(fitted(lmod),abs(residuals(lmod)),xlab="Fitted Values",ylab="|Residuals|")
 abline(h=0)
+summary(lm(abs(lmod$residual)~lmod$fitted))
+
+par(mfrow=c(1,2))
+plot(fitted(lmod),residuals(lmod),xlab="Fitted Values",ylab="Residuals")
+abline(h=0) # a diagnostic residual plot
+plot(fitted(lmod),sqrt(abs(residuals(lmod))), xlab="Fitted Values",ylab= expression(sqrt(hat(epsilon))))
+abline(h=0)
+
 
 # discover which predictor has a non-linear relationship with the response 
 par(mfrow=c(2,2))
@@ -32,15 +40,22 @@ plot(fitted(lmod2),residuals(lmod2),xlab="Fitted Values",ylab="Residuals")
 abline(h=0)
 
 par(mfrow=c(1,2))
-plot(fitted(lmod),residuals(lmod),xlab="Fitted Values",ylab="Residuals",main ="original model")
-abline(h=0)
-plot(fitted(lmod2),residuals(lmod2),xlab="Fitted Values",ylab="Residuals",main = "new model")
+plot(fitted(lmod2),residuals(lmod2),xlab="Fitted Values",ylab="Residuals")
+abline(h=0) # a diagnostic residual plot
+plot(fitted(lmod2),sqrt(abs(residuals(lmod2))), xlab="Fitted Values",ylab= expression(sqrt(hat(epsilon))))
 abline(h=0)
 par(mfrow=c(1,1))
 
 # constant variance
-summary(lm(abs(lmod$residual)~lmod$fitted.values))
-summary(lm(abs(lmod2$residual)~lmod2$fitted.values))
+summary(lm(sqrt(abs(lmod$residual))~lmod$fitted.values))
+summary(lm(sqrt(abs(lmod2$residual))~lmod2$fitted.values))
+
+## check correlated errors
+n <- length(residuals(lmod2))
+plot(tail(residuals(lmod2),n-1) ~ head(residuals(lmod2),n-1), xlab=
+       expression(hat(epsilon)[i]),ylab=expression(hat(epsilon)[i+1]))
+abline(h=0,v=0,col=grey(0.75))
+sumary(lm(tail(residuals(lmod2),n-1) ~ head(residuals(lmod2),n-1) -1))
 
 ## Check the normality assumption.
 qqnorm(lmod2$residuals,ylab = "Residuals")
@@ -48,15 +63,23 @@ qqline(lmod2$residuals)
 shapiro.test(lmod2$residuals)
 ## Check for large leverage points.
 ## half-normal plot for leverages
+par(mfrow=c(1,2))
 halfnorm(lm.influence(lmod2)$hat,nlab=2,ylab = "Leverage")
-sat[c(7,44),]
+abline(h=2*(5+1)/50)
+qqnorm(rstandard(lmod))
+abline(0,1)
 ## Check for outliers.
 ti<-rstudent(lmod2)
-max(abs(ti))
-which(ti==max(abs(ti)))
-2*(1-pt(max(abs(ti)),df=50-4-1))
+sat[which.max(abs(ti)),]
+2*(1-pt(max(abs(ti)),df=50-(5+1)-1))
 0.05/50
 
-
-
-
+summary(lm(rstandard(lmod2)~lmod2$fitted.values))
+### 2. (b) studentized residual instead of standard residuals
+par(mfrow=c(1,3))
+plot(fitted(lmod2),residuals(lmod2),xlab="Fitted Values",ylab="Residuals")
+abline(h=0)
+plot(fitted(lmod2),rstudent(lmod2),xlab="Fitted Values",ylab="Studentized Residuals")
+abline(h=0)
+plot(fitted(lmod2),rstandard(lmod2),xlab="Fitted Values",ylab="Standard Residuals")
+abline(h=0) # a diagnostic residual plot
